640d6497c3c2fd0a695406dfb8a498f68e48f546
ibbyhibby <ibrahim.allstar@gmail.com>
2024-11-03 23:14:28 -0500

    succesfully added authors, and confirmed working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [RouterLink, RouterOutlet]
+    imports: [RouterLink, RouterOutlet, FormsModule]
 })
 export class AppComponent {
   title = 'book-store';
